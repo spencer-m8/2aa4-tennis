85e9384eefa6602040eb617bfd65d428f2380a53
spencer-m8 <76497525+spencer-m8@users.noreply.github.com>
2024-01-11 15:45:07 -0500

    logger added, basic tennis loop -- bad practice, committing to fix branch

--- src/main/java/ca/mcscert/se2aa4/demos/tennis/Player.java
+++ src/main/java/ca/mcscert/se2aa4/demos/tennis/Player.java
@@ -1,2 +1,27 @@
-package ca.mcscert.se2aa4.demos.tennis;public class player {
+package ca.mcscert.se2aa4.demos.tennis;
+
+public class Player {
+    int strength;
+    int point = 0;
+    int set = 0;
+    String name;
+    Player(String name, int strength) {
+        this.name = name;
+        this.strength = strength;
+    }
+
+    public void scorePoint() {
+            this.point++;
+    }
+
+    public void scoreSet() {
+        if (this.set != 7) {
+            this.set++;
+        }
+    }
+
+    public void setPoint(int point) {
+        this.point = point;
+    }
+
 }
